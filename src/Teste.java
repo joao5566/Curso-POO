435e1e00e0af0061072ea8f4383dc717227e5e5a
jvrm <joao201566@gmail.com>
2022-04-11 23:00:00 -0300

    criei a conta C2 e transferi 10 da C1 para C2

--- src/Teste.java
+++ src/Teste.java
@@ -4,7 +4,10 @@ public class Teste {
 	public static void main(String[] args) {
 		Agencia ag = new Agencia(1);
 		Conta c1 = new Conta();
+		Conta c2 = new Conta();
+		c1.deposita(100);
 		
+		c1.transferir(c1, 10);
 		System.out.println("Codigo da Agencia: "+ ag.getCodigo());
 	}
 
