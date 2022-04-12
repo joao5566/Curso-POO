01f1d69124d1f274f931578629babb2e3d62eb3e
jvrm <joao201566@gmail.com>
2022-04-11 22:27:03 -0300

    adicionei uma nova conta

--- src/Teste.java
+++ src/Teste.java
@@ -3,7 +3,9 @@ public class Teste {
 
 	public static void main(String[] args) {
 		Agencia ag = new Agencia(1);
+		Conta c1 = new Conta();
 		
+		c1.deposita(100);	
 		System.out.println("Codigo da Agencia: "+ ag.getCodigo());
 	}
 
