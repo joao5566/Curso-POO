24a743cc4aa264043f2c9233413d43d10a2e6b67
jvrm <joao201566@gmail.com>
2022-04-12 23:13:44 -0300

    recuperei os dados

--- src/Teste.java
+++ src/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 	public static void main(String[] args) {
 		Cliente cli = new Cliente();
 		cli.setNome("João Vitor Rodrigues");
-		Conta c = new Conta(cli,2500);
+		Conta c = new Conta(cli,2100);
 		
 		System.out.println("Cliente " + c.getTitular().getNome());
 		System.out.println("Limite " + c.getLimite());
