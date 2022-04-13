3e55283629cc604a1502d42458b6686bb9bab314
jvrm <joao201566@gmail.com>
2022-04-12 23:09:30 -0300

    erro corrigido

--- src/Teste.java
+++ src/Teste.java
@@ -2,13 +2,8 @@
 public class Teste {
 
 	public static void main(String[] args) {
-		Agencia ag = new Agencia(1);
-		Conta c1 = new Conta();
-		Conta c2 = new Conta();
-		c1.deposita(100);
 		
-		c1.transferir(c1, 10);
-		System.out.println("Codigo da Agencia: "+ ag.getCodigo());
+		System.out.println("Codigo da Agencia: ");
 	}
 
 }
