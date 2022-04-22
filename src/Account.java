1fc2efee8d3fa31b3e0a4676a75df1421bfedbc8
jvrm <joao201566@gmail.com>
2022-04-21 22:01:04 -0300

    I added the deposit method and the balance was also added to the account

--- src/Account.java
+++ src/Account.java
@@ -2,15 +2,29 @@
 //Account class that contains an instance variable name
 //and methods to configure and obtain its value.
 
-
 public class Account 
 {
 	private String name;//instance variable
-	// the constructor name initializes with the parameter name
-	public Account(String name) 
+	private double balance;//instance variable
+
+	// Account constructor that takes two parameters
+	public Account(String name, double balance) 
 	{
 		this.name = name;
+
+		// validate that the balance is greater than 0.0; if not,
+		// the balance instance variable keeps its initial default value of 0.0
+		if (balance > 0.0)// if the balance is valid
+			this.balance = balance;// assign it to the balance instance variable
 	}
+
+	// method that deposits (adds) only a valid amount to the balance
+	public void deposit(double depositAmount)
+	{
+		if(depositAmount > 0.0)//if the depostAmount is valid
+			balance = balance + depositAmount;//add it to the balance	
+	}
+
 	// method to set the name on the object
 	public void setname(String name) 
 	{
@@ -22,4 +36,9 @@ public class Account
 		return name;//returns the name value to the caller
 	}
 	
+	public double getBalance()
+	{
+		return balance;
+	}
+
 }//end of class Account
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -1,19 +1,49 @@
-// Figura 3.6: AccountTest.Java
-// Usando o construtor de Account para inicializar a instância name
-// variável no momento em que cada objeto Account é criado.
+// Figure 3.6: AccountTest.Java
+// Using Account's constructor to initialize instance name
+// variable at the time each Account object is created.
+
+import java.util.Scanner;
 
 public class AccountTest
 {
 	public static void main(String[] args)
-{
-	// cria dois objetos Account
-	Account account1 = new Account("Jane Green");
-	Account account2 = new Account("John");
+	{
+		// create two Account objects
+		Account account1 = new Account("Jane Green",50.00);
+		Account account2 = new Account("John",-7.53);
 
-	// exibe o valor inicial de nome para cada Account
-	System.out.printf("account1 name is: %s%n", account1.getName());
+		// display the initial value of name for each Account
+		System.out.printf("%s balance: $%.2f %n",
+			account1.getName(),account1.getBalance());
+		System.out.printf("%s balance: $%.2f %n%n",
+			account2.getName(),account2.getBalance());
 
-	System.out.printf("account2 name is: %s%n", account2.getName());
-}
+		// create a Scanner to get input from the command window
+		Scanner input = new Scanner(System.in);
 
-} // fim da classe AccountTest
+		System.out.print("Enter deposit amount for account1: ");//prompt
+		double depositAmount = input.nextDouble();// get user input
+		System.out.printf("%nadding %.2f to account1 balance%n%n",
+			depositAmount);
+		account1.deposit(depositAmount);// add account1 balance
+
+		// display balances
+		System.out.printf("%s balance: $%.2f %n",
+			account1.getName(),account1.getBalance());
+		System.out.printf("%s balance: $%.2f %n",
+			account2.getName(),account2.getBalance());
+
+		System.out.print("Enter deposit amount for account2: ");//prompt
+		depositAmount = input.nextDouble();// get user input
+		System.out.printf("%nadding %.2f to account2 balance%n%n",
+			depositAmount);
+		account2.deposit(depositAmount);// add account2 balance
+
+		// display balances
+		System.out.printf("%s balance: $%.2f %n",
+			account1.getName(),account1.getBalance());
+		System.out.printf("%s balance: $%.2f %n",
+			account2.getName(),account2.getBalance());
+	}
+
+} // end of AccountTest class
